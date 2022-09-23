76eba7f3b352ca5fe718635fe94c2dea7ae2bb16
Ivan Husarov <ivan.husarov@gmail.com>
2022-09-23 17:42:57 +0300

    subgraph

--- src/po-pbase.ts
+++ src/po-pbase.ts
@@ -1,4 +1,5 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ipfs, json } from "@graphprotocol/graph-ts"
+
 import {
   POPbase,
   Approval,
@@ -6,61 +7,43 @@ import {
   OwnershipTransferred,
   Transfer
 } from "../generated/POPbase/POPbase"
-import { ExampleEntity } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import {ConnectionNFT, User} from '../generated/schema'
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+export function handleTransfer(event: Transfer): void {
+  let connectionNFT = ConnectionNFT.load(event.params.tokenId.toString())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if(!connectionNFT) {
+    connectionNFT = new ConnectionNFT(event.params.tokenId.toString())
+    connectionNFT.creator = event.transaction.from.toHexString()
+    connectionNFT.tokenID = event.params.tokenId;
+    connectionNFT.createdAtTimestamp = event.block.timestamp;
+
+    const popBaseContract = POPbase.bind(event.address)
+    connectionNFT.contentURI = popBaseContract.tokenURI(event.params.tokenId)
+    connectionNFT.metadataURI = popBaseContract.tokenURI(event.params.tokenId)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // const ipfsData = ipfs.cat(connectionNFT.metadataURI.split("/").at(-1))
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
+  // if(ipfsData){
+  //   const jsonIpfsData = json.fromBytes(ipfsData).toObject()
+  //    jsonIpfsData.get("request") 
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  // }
+  connectionNFT.owner = event.params.to.toHexString()
+  connectionNFT.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+  let fromUser = User.load(event.transaction.from.toHexString())
+  if(!fromUser) {
+    fromUser = new User(event.transaction.from.toHexString())
+    fromUser.save()
+  }
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.mintAndTransfer(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
+  let toUser = User.load(event.params.to.toHexString())
+  if(!toUser) {
+    toUser = new User(event.params.to.toHexString())
+    toUser.save()
+  }
+  
 }
-
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransfer(event: Transfer): void {}
